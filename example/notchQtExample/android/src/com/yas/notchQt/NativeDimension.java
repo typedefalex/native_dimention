ef53dbc5b031bdcaa62bc4f91dcdad3e4baa1657
Alex Yakubinkiy <prktown@gmail.com>
2019-04-12 18:44:55 +0700

    added check for layoutInDisplayCutoutMode

--- example/notchQtExample/android/src/com/yas/notchQt/NativeDimension.java
+++ example/notchQtExample/android/src/com/yas/notchQt/NativeDimension.java
@@ -20,9 +20,13 @@ public class NativeDimension extends QtActivity
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-                getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+		    getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+
+
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+		getWindow().setStatusBarColor(Color.TRANSPARENT);
             }
             else {
                 getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
